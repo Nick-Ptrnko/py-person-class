aadeb2505917a965c1e1e12517749a1f618f7c07
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-10-22 16:05:49 +0300

    Перш. прикидки

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 class Person:
-    # write your code here
-    pass
-
+    people = {}
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        # Додаємо екземпляр до атрибута класу people
+        Person.people[self.name] = self
+        #people[self.name] = self
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    for person in people:
+        person_list.append(Person(person, person.age))
+
+people = [
+    {"name": "Ross", "age": 30, "wife": "Rachel"},
+    {"name": "Joey", "age": 29, "wife": None},
+    {"name": "Rachel", "age": 28, "husband": "Ross"}
+]
+person_list = create_person_list(people)
