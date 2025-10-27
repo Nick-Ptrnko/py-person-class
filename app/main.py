1cef85f9c1a3eb670a69ff9450c83425b1dca589
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-10-27 12:11:19 +0200

    new person list

--- app/main.py
+++ app/main.py
@@ -5,16 +5,16 @@ class Person:
         self.age = age
         # Додаємо екземпляр до атрибута класу people
         Person.people[self.name] = self
-        #people[self.name] = self
 
 def create_person_list(people: list) -> list:
     person_list = []
     for person in people:
-        person_list.append(Person(person, person.age))
-
+        person_list.append(Person(person["name"], person["age"]))
+    return person_list
 people = [
     {"name": "Ross", "age": 30, "wife": "Rachel"},
     {"name": "Joey", "age": 29, "wife": None},
     {"name": "Rachel", "age": 28, "husband": "Ross"}
 ]
-person_list = create_person_list(people)
+person_list_1 = create_person_list(people)
+print(person_list_1)
