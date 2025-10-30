6a651e4e4e5510fbce3d8492f0a83145371c7e0f
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-10-30 17:03:57 +0200

    solution 2

--- app/main.py
+++ app/main.py
@@ -9,10 +9,7 @@ class Person:
 
 def create_person_list(people_dicts: list) -> list:
     person_list = []
-    for person_dict in people_dicts:
-        person_list.append(
-            Person(person_dict["name"], person_dict["age"])
-        )
+    person_list = [Person(person_dict["name"], person_dict["age"]) for person_dict in people_dicts]
 
     for person_dict in people_dicts:
 
